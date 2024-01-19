a0425fbaefe25e76474a7af5960d6621b20c4882
barucb <130171616+barucb@users.noreply.github.com>
2024-01-19 22:58:02 +0300

    Update App.tsx gdd

--- src/App.tsx
+++ src/App.tsx
@@ -72,7 +72,7 @@ const App = () => {
 
     const handleCopyUrl = () => {
         navigator.clipboard.writeText(currentUrl);
-        toast.success(`Link Copied Successfully! You can Share it to ${searchParams.get('to')} now`);
+        toast.success(`ሊንኩ ኮፒ ተደርጓል::  ለ ${searchParams.get('to')} ይላኩትይላኩት`);
     };
 
     return (
@@ -241,8 +241,9 @@ const App = () => {
                             <Link href="/" className="text-lg w-14  flex justify-center m-2 h-14 w text-center text-black bg-gradient-to-l from-lime-500 to-yellow-400 mr-6 rounded-lg px-1  max-w-[100px] mt-16 ">
                                 አዲስ ይዘዙ
                             </Link>
-                            <button className="bg-gradient-to-l from-lime-500 to-yellow-400 w-14 flex justify-center m-2 h-14 items-center rounded-lg  px-1  max-w-[100px] mt-16" onClick={handleCopyUrl}>
-                                <svg className="h-10 " xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" ><circle cx="18" cy="5" r="3" /><circle cx="6" cy="12" r="3" /><circle cx="18" cy="19" r="3" /><line x1="8.59" x2="15.42" y1="13.51" y2="17.49" /><line x1="15.41" x2="8.59" y1="6.51" y2="10.49" /></svg>
+                            <button className="bg-gradient-to-l text-lg from-lime-500 to-yellow-400 w-14 flex justify-center m-2 h-14 items-center rounded-lg  px-1  max-w-[100px] mt-16" onClick={handleCopyUrl}>
+                                ሊንኩን ያጋሩ
+{/*                                 <svg className="h-10 " xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" ><circle cx="18" cy="5" r="3" /><circle cx="6" cy="12" r="3" /><circle cx="18" cy="19" r="3" /><line x1="8.59" x2="15.42" y1="13.51" y2="17.49" /><line x1="15.41" x2="8.59" y1="6.51" y2="10.49" /></svg> */}
                                 {/* <img className="h-  m-2" src={share} alt="Share" /> */}
 
                             </button>
